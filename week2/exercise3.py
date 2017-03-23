3f2693bd8969c45950247bdbc3c4c9504c02189a
Rizo007 <hypervenoms007@outlook.com>
2017-03-23 18:21:14 +1100

    Add files via upload

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -247,7 +247,18 @@ def loops_7():
     This is a hard problem. Use lots of experimentation and draw
     lots of diagrams!
     """
-    pass
+    columns = []
+    for x in range(5):
+        rows = []
+        for y in range(9):
+            if abs(y-4) <= x:
+                rows.append('*')
+            else:
+                rows.append(' ')
+        columns.append(rows)
+
+    print(columns)
+    return columns
 
 
 def lp(some_kind_of_list, exercise_name):
